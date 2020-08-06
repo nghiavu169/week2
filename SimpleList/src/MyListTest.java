db9558fb935099f613a23af9f936c1d75ce06138
nghiavu169 <vunghia2000@gmail.com>
2020-08-06 09:54:22 +0700

    ok

---
+++ SimpleList/src/MyListTest.java
@@ -0,0 +1,10 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Object> listInteger = new MyList<>();
+        listInteger.add(1);
+        listInteger.add("a");
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+    }
+}
